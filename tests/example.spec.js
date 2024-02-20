c9482c449813a53c11fb72d7420086ac9b1e40ac
AnkitaNitor <ankita.prabhu@nitorinfotech.com>
2024-02-20 17:49:57 +0530

    PR check 1

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,7 +19,7 @@ test('get started link', async ({ page }) => {
 })
 
 test(`hello world`, async({page}) =>{
-  await console.log(`Hello Jhioooooooooo jdcdcksjddcccccn mcccccccwndndmdmedd`);
+  await console.log(`Hello Jhioooooooooo jdcdcksjddccccccccccccccccccccccn mcccccccwndndmdmedd`);
 })
 
 test(`hello worldnhdjwhdwj`, async({page}) =>{
