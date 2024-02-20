cc9fc9ec39fbeee5b2ec6f2f434ebf3e6c8aa88e
AnkitaNitor <ankita.prabhu@nitorinfotech.com>
2024-02-20 12:06:53 +0530

    fun git PR

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,7 +19,7 @@ test('get started link', async ({ page }) => {
 })
 
 test(`hello world`, async({page}) =>{
-  await console.log(`Hello Jhioooooooooo jdcdcksjddn mwndndmdmedd`);
+  await console.log(`Hello Jhioooooooooo jdcdcksjddn mcccccccwndndmdmedd`);
 })
 
 test(`hello worldnhdjwhdwj`, async({page}) =>{
